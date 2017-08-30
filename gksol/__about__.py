e50c3e713c92a8a95a6a1a68beaf1686098fe48c
Max Fischer <maxfischer2781@gmail.com>
2017-08-30 14:28:02 +0200

    version bump

--- gksol/__about__.py
+++ gksol/__about__.py
@@ -2,7 +2,7 @@ __title__ = 'gksol'
 __summary__ = 'Game of Life Tutorial Implementation and Framework'
 __url__ = 'https://github.com/MaineKuehn/gksol'
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Eileen Kuehn, Max Fischer'
 __email__ = 'maxfischer2781@gmail.com'
 __copyright__ = '2017 %s' % __author__
